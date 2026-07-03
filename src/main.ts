39cf10101078b4b872df43625653d6519772a786
Zak Griffin <zakgriffin1209@gmail.com>
2026-07-02 23:45:49 -0400

    Edit src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import * as SkeletonUtils from 'three/addons/utils/SkeletonUtils.js';
+import jsQR from 'jsqr';
 
 // ── Math helpers ──────────────────────────────────────────────────────────────
 
